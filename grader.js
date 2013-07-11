aa36ebd23d0aee47ad155f7ce65bbccfee15f7e3
Kekeli <kekeli.scott@gmail.com>
2013-07-11 23:24:25 +0000

    Finish cleanup, fix default url, add verbose option

--- grader.js
+++ grader.js
@@ -27,12 +27,12 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://html5test.com/index.html";
+var URL_DEFAULT = "http://blooming-sands-7854.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
+        console.log("Error: File %s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
@@ -43,30 +43,6 @@ var assertUrlValid = function(url) {
     return url.toString();;
 };
 
-
-// asynch
-var checkUrl = function(url, checksfile) {
-    request = rest.get(url);
-    request.addListener('error', function (data) {
-	    console.log("Invalid url %s. Exiting.", url);
-	    process.exit(1);
-    });
-    request.addListener('success', function(data) {
-	    console.log("Retrieved html");
-
-	$ = cheerioUrl(data); 
-	var checks = loadChecks(checksfile).sort();
-	var out = {};
-	for(var ii in checks) {
-            var present = $(checks[ii]).length > 0;
-            out[checks[ii]] = present;
-	}
-
-	var outJson = JSON.stringify(out, null, 4);
-	console.log(outJson);
-    });
-};
-
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -75,22 +51,17 @@ var cheerioUrl = function(html) {
     return cheerio.load(html);
 }
 
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
-};
-
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+
+    return checkHtml($, checksfile);
 };
 
-// todo: callback
+var checkUrl = function(data, checksfile) {
+	$ = cheerioUrl(data); 
+	return checkHtml($, checksfile);
+};
+
 var checkHtml = function(cheerioInput, checksfile) {
     $ = cheerioInput;
     var checks = loadChecks(checksfile).sort();
@@ -108,25 +79,35 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-u, --url <url>', 'Url of file to check', clone(assertUrlValid), URL_DEFAULT)
+        .option('-v, --verbose', 'Verbose mode')
         .parse(process.argv);
 
-    console.log("Checks: %s", program.checks);
-    console.log("Url: %s", program.url);
-    console.log("File: %s", program.file);
-
+    if (program.verbose) {
+	console.log("Input");
+	console.log("  Checks: %s", program.checks);
+	console.log("  Url: %s", program.url);
+	console.log("  File: %s", program.file);
+    }
     if (program.url) {
-
-	// todo: need to do the outJson stuff on complete of checkUrl...
-
-	var checkJson = checkUrl(program.url, program.checks);
-	//var outJson = JSON.stringify(checkJson, null, 4);
-	//console.log(outJson);
+	rest.get(program.url)
+	    .on('success', function(result) {
+		var checkJson = checkUrl(result, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	    })
+	    .on('error', function(result) {
+		console.log("Error: Invalid url %s. Exiting.", program.url);
+		process.exit(1);
+	    });
     }
     else {
 	var checkJson = checkHtmlFile(program.file, program.checks);
@@ -135,4 +116,5 @@ if(require.main == module) {
     }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkUrl = checkUrl;
 }
