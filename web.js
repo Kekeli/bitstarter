f7fa9d7c340cedb239162519110cc30639e8936a
Kekeli <kekeli.scott@gmail.com>
2013-07-22 11:11:28 +0000

    Add knot image, enable static content in web.js, fix typos

--- web.js
+++ web.js
@@ -8,6 +8,8 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
+app.use(express.static('/home/ubuntu/bitstarter'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
